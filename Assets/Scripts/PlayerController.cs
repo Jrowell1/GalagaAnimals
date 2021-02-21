93cbeed8107516cf7a3e364244a9a460595a3153
Brandon King <kingb18@students.ecu.edu>
2021-02-21 10:37:31 -0500

    Updated Background and Removed Projectiles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 20.0f;
-    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -26,10 +25,5 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-
-        if(Input.GetKeyDown(KeyCode.Space)){
-            //Launch projective from the player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
     }
 }
