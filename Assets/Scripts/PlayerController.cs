7e0b1d14e6dca5c1cbcccb712fe1728f82ffd3c2
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-02-20 20:13:38 -0500

    Added public variables to store health

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,11 @@ public class PlayerController : MonoBehaviour
     public float xRange = 20.0f;
     public GameObject projectilePrefab;
 
+    //Health Tracking
+    public float health = 100;
+    public float foodHealth = 10;
+    public float animalDamage = 25;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(health <= 0){
+            //END GAME CONDTIONS
+            Debug.Log("GAME OVER!!");
+            Destroy(gameObject);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
 
         if(transform.position.x < -xRange){
@@ -31,5 +42,17 @@ public class PlayerController : MonoBehaviour
             //Launch projective from the player
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
+
+        //Add Health Tracking, Animals do damage and Food increase health 
+
+        //Get Collisions, Food Tag or Animal tag will determine change
+        //Psudep
+        /*if collisionObject.tag == "animal"
+            health - animalDamage;
+        else if collisionObject.tag == "food"
+            health + foodHealth;
+        */
+
+        
     }
 }
