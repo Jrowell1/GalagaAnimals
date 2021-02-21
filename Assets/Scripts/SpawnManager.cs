ee93e02917051056ae496f3da5ed94063f75758f
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-02-20 19:32:52 -0500

    Adding Assets

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private float spawnRangex = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    public GameObject[] animalPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(Input.GetKeyDown(KeyCode.S)){
+            SpawnRandomAnimal();
+        }*/
+
+        
+    }
+
+    void SpawnRandomAnimal(){
+        int animalIndex = Random.Range(0,animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangex,spawnRangex + 1),0,spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
